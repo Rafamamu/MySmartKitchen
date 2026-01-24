71b387689244d31ef664bb662fe4d8aac92d6ca8
Rafael <rafaroga46@gmail.com>
2026-01-24 14:23:44 -0300

    First commit

---
+++ src/main/java/com/gmail/rafaroga46/MySmartKitchen/MySmartKitchenApplication.java
@@ -0,0 +1,14 @@
+package com.gmail.rafaroga46.MySmartKitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySmartKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySmartKitchenApplication.class, args);
+		System.out.println("Pronto para rodar...");
+	}
+
+}
---
+++ src/test/java/com/gmail/rafaroga46/MySmartKitchen/MySmartKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.rafaroga46.MySmartKitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySmartKitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
