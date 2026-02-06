89d458ae647eb5731455c5132f5ca5a664c7c122
Rafael <rafaroga46@gmail.com>
2026-02-05 23:58:42 -0300

    feat: Method in waiter service to create completed waiter.

--- src/main/java/com/gmail/rafaroga46/MySmartKitchen/service/GarcomService.java
+++ src/main/java/com/gmail/rafaroga46/MySmartKitchen/service/GarcomService.java
@@ -1,14 +1,29 @@
 package com.gmail.rafaroga46.MySmartKitchen.service;
 
+import com.gmail.rafaroga46.MySmartKitchen.controller.request.CreateGarcomRequest;
+import com.gmail.rafaroga46.MySmartKitchen.controller.request.response.GarcomResponse;
+import com.gmail.rafaroga46.MySmartKitchen.entity.Garcom;
+import com.gmail.rafaroga46.MySmartKitchen.mapper.GarcomMapper;
 import com.gmail.rafaroga46.MySmartKitchen.repository.GarcomRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
 public class GarcomService {
 
-    private GarcomRepository garcomRepository;
+    private final  GarcomRepository garcomRepository;
+    private final GarcomMapper mapper;
 
-    public GarcomService(GarcomRepository garcomRepository) {
+    public GarcomService(GarcomRepository garcomRepository, GarcomMapper mapper) {
         this.garcomRepository = garcomRepository;
+        this.mapper = mapper;
     }
+
+
+    public GarcomResponse criarGarcom(CreateGarcomRequest request) {
+        return GarcomMapper.toResponse(
+                garcomRepository.save(GarcomMapper.toEntity(request)));
+
+    }
+
 }
